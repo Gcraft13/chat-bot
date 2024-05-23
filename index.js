fbc06da82bb3b93ba703c80983f7e7c4fd0856c0
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-05-23 12:24:51 -0400

    q

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ const recognition = new SpeechRecognition();
 document.querySelector("button").addEventListener("click", () => {
   recognition.start();
 });
+
+recognition.addEventListener("result", (e) => {
+  let last = e.results.lengtyh - 1;
+  let text = e.restults[last][0].transcript;
+
+  socket.emit("chat message", text);
+
+  console.log("Confidence" + e.results[0][0].confidence);
+});
